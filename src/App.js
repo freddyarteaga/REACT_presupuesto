26a6ff2c7ff10a7c1e16ae8089a9d16ae24212d5
Freddy <freddy.artea@gmail.com>
2020-02-10 20:15:54 -0500

    ajustes iniciales y Css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+     <div className='container'>
+       <header>
+       <h1>Gasto Semanal</h1>
+       </header>
+     </div>
+    );
+  }
+}
+
+export default App;
